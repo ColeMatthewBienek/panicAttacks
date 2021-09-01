ff256fd8397421b0f017afb3fe316d3e7252b033
Maia Ling <electric.pen@gmail.com>
2021-08-31 23:58:00 -0700

    Refactored code to be cleaner

--- decodeMorse.js
+++ decodeMorse.js
@@ -53,66 +53,25 @@ var MORSE_CODE = {
   '-.--': 'Y',
   '--..': 'Z',
   '..--.-': '_',
-  '...---...': 'SOS' 
+  '...---...': 'SOS'
 }
 
+var getWordList = morseCode => morseCode.trim()
+                                        .split('   ')
 
-//split at the three space delimiter
-function splitAtThree (morseCode) {
-    console.log('split at three: ',morseCode.split(/ {3}/g))
-    return morseCode.split(/ {3}/g)
-}
+var decodeWords = morseWords => morseWords.map(decodeWord)
+                                          .join(' ')
 
-//split the words into letters
-function splitWords (morseCode) {
-    var letters = []
-    morseCode.forEach(function(words) {
-       letters.push(words.split(' '))
-    })
-    console.log('split words ',letters)
-    return letters
-}
+var decodeWord = codedWord => codedWord.split(' ')
+                                       .map(decodeLetter)
+                                       .join('')
 
+var decodeLetter = codedLetter => MORSE_CODE[codedLetter]
 
-//translate the letters
-function translateMorse (morseCode) {
-   var translated= []
-   morseCode.forEach(function(word){
-       translated.push(MORSE_CODE[word])
-   })
-    
-    return translated
-}
-
-function sentenceBuild (words) {
-    var waitForIt = []
-    words.forEach(function(ele) {
-        waitForIt.push(ele.join(''))
-    })
-    console.log('Wait...',waitForIt)
-    return waitForIt
-}
 
 //controlling function=>will call the others
-function decodeMorse (morseCode) {
-    //split the symbls at word breaks
-    var decode = splitAtThree(morseCode)
-    
-    //split words into letters
-    var splitIntoLetters = splitWords(decode)
-    
-    //translate and store words as seperate elements
-    var almostDone = []
-    splitIntoLetters.forEach(function(word){
-        almostDone.push(translateMorse(word))
-    })
-   
-    //clean up the sentence and join the array with a space between elements
-    var nearlyThere = sentenceBuild(almostDone) 
-    var allDone = nearlyThere.join(' ')
-    console.log('maybe...',allDone)
-    return allDone.trim()
-}
+var decodeMorse = morseCode => decodeWords(getWordList(morseCode))
 
 
-decodeMorse('.... . -.--   .--- ..- -.. .')
+
+console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
