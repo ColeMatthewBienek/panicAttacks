bf0638136a9ddbb671293928a75174d9efc1e04a
Maia Ling <electric.pen@gmail.com>
2021-09-01 00:05:08 -0700

    Added output descriptions to comments

--- decodeMorse.js
+++ decodeMorse.js
@@ -57,22 +57,27 @@ var MORSE_CODE = {
 }
 
 // Splits a morse code sentence into an array of individual morse-encoded words
+// returns the array of morse-words
 var getWordList = morseCode => morseCode.trim()
                                         .split('   ')
 
-// Replaces the encoded words with plain-english words
+// Replaces each encoded word in an array with plain-english words
+// returns the modified array
 var decodeWords = morseWords => morseWords.map(decodeWord)
                                           .join(' ')
 
 // Decodes a single word from morse code to plain english
+// returns a plain english word
 var decodeWord = codedWord => codedWord.split(' ')
                                        .map(decodeLetter)
                                        .join('')
 
 // Translates a single morse code element to an english character
+// returns an english character
 var decodeLetter = codedLetter => MORSE_CODE[codedLetter]
 
 // Translates a morse-encoded sentence into plain english
+// returns a sentence in english
 var decodeMorse = morseCode => decodeWords(getWordList(morseCode))
 
 
